9ddabe331ef2a2bbd21a012857cd65c3c9d354a6
Tyler Dary <daryt@msn.com>
2014-09-29 14:41:28 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Blocitoff::Application.routes.draw do
+  get "static/index"
   resources :task_items
 
   devise_for :users
   resources :task_items, only: [:new, :create, :show]
-  root to: 'task_items#new'
+  #root to: 'task_items#new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
